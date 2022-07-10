b147636eee46a4458f52bbd471b85f5f011d32b6
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-07-10 16:09:18 +0000

    Add comments for users in get_input_url and get_page_html.

--- run.py
+++ run.py
@@ -7,20 +7,25 @@ def get_input_url():
     print("Are you ready to scrape a webpage?")
     page_link = input("Enter the url you want to scrape:\n")
 
-    return page_link
+    print(f"Thank you! I'm scraping {page_link}...\n")
 
+    return page_link
 
 def get_page_html(page_link):
     """
     Send a get HTTP request to page_link and receive the html of the page.
     raise_for_status verifies that the request is good.
     """
+    print("Parsing the page html...\n")
     response = requests.get(page_link)
     response.raise_for_status()
     page_html = bs4.BeautifulSoup(response.text, "html.parser")
     print(page_html)
 
-def main():    
+def main():
+    """ 
+    Run all the program functions.
+    """
     page_link = get_input_url()
     page_html = get_page_html(page_link)
 
