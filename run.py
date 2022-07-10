0ebc97e6b6980a3cd873594a320a6188715188f2
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-07-10 14:38:20 +0000

    Add function get_page_html doctring.

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import bs4, requests
 
 page_link = "https://pietralikelocals.com/"
 
-def get_page_html():
+def get_page_html(page_link):
+    """
+    Send a get HTTP request to page_link and receive the html of the page.
+    raise_for_status verifies that the request is good.
+    """
     response = requests.get(page_link)
     response.raise_for_status()
     page_html = bs4.BeautifulSoup(response.text, "html.parser")
 
-page_html = get_page_html()
+page_html = get_page_html(page_link)
