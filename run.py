98010e2eb04d6f3b4ddd611d766d724f9702b44f
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-07-10 14:12:15 +0000

    Create get_page_html() function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import bs4, requests
+
+page_link = "https://pietralikelocals.com/"
+
+def get_page_html():
+    response = requests.get(page_link)
+    response.raise_for_status()
+    page_html = bs4.BeautifulSoup(response.text, "html.parser")
+
+page_html = get_page_html()
